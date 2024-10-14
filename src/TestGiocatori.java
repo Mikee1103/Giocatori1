58f175ab58e3ef71eef56ee4ff375e932986f2d4
seghezzi.21139 <seghezzi.michele.studente@itispaleocapa.it>
2024-10-14 12:59:52 +0200

    2

--- src/TestGiocatori.java
+++ src/TestGiocatori.java
@@ -8,16 +8,18 @@ public class TestGiocatori {
         int indice = 0;
         int indice2 = 0;
         int goal;
+        int giocator;
         String nome;
         Boolean capitano;
         do {
+            System.out.println(" ");
             System.out.println("1 - aggiungere un giocatore alla squadra");
-            System.out.println("1 - visualizzare tutti i giocatori");
-            System.out.println("1 - modificare i dati di un giocatore");
-            System.out.println("1 - cancellare un giocatore dalla squadra");
-            System.out.println("1 - visualizzare i giocatori che hanno realizzato più di 5 goal.");
-            System.out.println("1 - visualizzare il capitano");
-            System.out.println("1 - assegnare il ruolo di capitano in modo casuale se non ancora presente");
+            System.out.println("2 - visualizzare tutti i giocatori");
+            System.out.println("3 - modificare i dati di un giocatore");
+            System.out.println("4 - cancellare un giocatore dalla squadra");
+            System.out.println("5 - visualizzare i giocatori che hanno realizzato più di 5 goal.");
+            System.out.println("6 - visualizzare il capitano");
+            System.out.println("7 - assegnare il ruolo di capitano in modo casuale se non ancora presente");
             System.out.println("8 - esci");
             indice2 = t.nextInt();
             switch (indice2) {
@@ -28,11 +30,39 @@ public class TestGiocatori {
                     System.out.println("inserisci nome");
                     nome = t.nextLine();
                     t.nextLine();
-                    System.out.println("dimmi se è capitano");
+                    System.out.println("è capitano? (true/false)");
                     capitano = t.nextBoolean();
                     t.nextLine();
                     aggiuntaGiocatore(goal, nome, capitano, indice);
                     indice++;
+                    break;
+                case 2:
+                    for (int i = 0; i < indice; i++) {
+                        System.out.println(stampa(i));
+                    }
+                    break;
+                case 3:
+                    System.out.println("scegli giocatore da modificare");
+                    giocator = t.nextInt();
+                    System.out.println("inserisci numero goal nuovo");
+                    goal = t.nextInt();
+                    t.nextLine();
+                    System.out.println("inserisci nuovo nome");
+                    nome = t.nextLine();
+                    t.nextLine();
+                    System.out.println("è capitano?(true/false)");
+                    capitano = t.nextBoolean();
+                    t.nextLine();
+                    modifica(giocator, goal, nome, capitano);
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
                     break;
             }
         }
@@ -40,10 +70,21 @@ public class TestGiocatori {
     }
     public static void aggiuntaGiocatore(int newGoal, String newNome, Boolean newCapitano, int indice){
         gioc [indice] = new Giocatori(newGoal,newNome,newCapitano);
-
-
-
+    }
+    public static String stampa(int i){
+        String giocatore="";
+        giocatore=giocatore+gioc [i].getNome() + "\t" + gioc [i].getGoal() + "\t" +gioc [i].getCapitano() + "\t";
+        return giocatore;
+    }
+    public static void modifica(int giocator, int newGoal, String newNome, Boolean newCapitano){
+        gioc[giocator].setGoal(newGoal);
+        gioc[giocator].setNome(newNome);
+        gioc[giocator].setCapitano(newCapitano);
 
 
     }
+    public void cancellazione(){
+
+    }
+
 }
